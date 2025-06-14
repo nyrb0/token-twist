1daa60467a5078331b8989c5952411d6b1bbff2e
Nurbolot <nubolotkadyrov06@gmail.com>
2025-06-15 00:19:34 +0600

    feat/footer

--- src/shared/footer/Footer.tsx
+++ src/shared/footer/Footer.tsx
@@ -6,55 +6,73 @@ import styles from './Footer.module.scss';
 
 const Footer = () => {
     return (
-        <footer className={`${styles.footer} df jcc`}>
-            <div className={`${styles.wrapper} df fdc jcsb`}>
-                <div className={`${styles.navigations} df jcsa ais`}>
-                    <button>Главная</button>
-                    <div className={`${styles.column} df fdc`}>
-                        <button>О нас</button>
-                        <button>Токеномика</button>
-                        <button>RoadMap</button>
+        <div className="container">
+            <footer className={`${styles.footer} df jcc`}>
+                <div className={`${styles.wrapper} df fdc jcsb `}>
+                    <div className={`${styles.navigations} `}>
+                        <div className={`${styles.navigationsDesktop} df jcsa ais`}>
+                            <button>Главная</button>
+                            <div className={`${styles.column} df fdc`}>
+                                <button>О нас</button>
+                                <button>Токеномика</button>
+                                <button>RoadMap</button>
+                            </div>
+                            <button>Покупка токена</button>
+                            <div className={`${styles.column} df fdc`}>
+                                <button>Соц-сети</button>
+                                <button>Наш Амбасадор</button>
+                                <button>Отзыв</button>
+                            </div>
+                            <button>FOQ</button>
+                        </div>
                     </div>
-                    <button>Покупка токена</button>
-                    <div className={`${styles.column} df fdc`}>
-                        <button>Соц-сети</button>
-                        <button>Наш Амбасадор</button>
-                        <button>Отзыв</button>
+                    <div className="df jcc">
+                        <div className={`${styles.mobileNavigations} ${styles.navigations} df fdc `}>
+                            <button>Главная</button>
+                            <button>О нас</button>
+                            <button>Токеномика</button>
+                            <button>RoadMap</button>
+                            <button>Покупка токена</button>
+                            <button>Соц-сети</button>
+                            <button>Наш Амбасадор</button>
+                            <button>Отзыв</button>
+                            <button>FOQ</button>
+                        </div>
                     </div>
-                    <button>FOQ</button>
+
+                    <div className={`${styles.socials} df jcsa `}>
+                        <div>
+                            <div className={styles.social}>
+                                <IconDiscord />
+                                Discord
+                            </div>
+                            <div className={styles.border}></div>
+                        </div>
+                        <div>
+                            <div className={styles.social}>
+                                <IconTelegtam />
+                                Telegram
+                            </div>
+                            <div className={styles.border}></div>
+                        </div>
+                        <div>
+                            <div className={styles.social}>
+                                <IconTiktok />
+                                TikTok
+                            </div>
+                            <div className={styles.border}></div>
+                        </div>
+                        <div>
+                            <div className={styles.social}>
+                                <IconTwitter />
+                                Twitter
+                            </div>
+                            <div className={styles.border}></div>
+                        </div>
+                    </div>
                 </div>
-                <div className={`${styles.socials} df jcsa`}>
-                    <div>
-                        <div className={styles.social}>
-                            <IconDiscord />
-                            Discord
-                        </div>
-                        <div className={styles.border}></div>
-                    </div>
-                    <div>
-                        <div className={styles.social}>
-                            <IconTelegtam />
-                            Telegram
-                        </div>
-                        <div className={styles.border}></div>
-                    </div>
-                    <div>
-                        <div className={styles.social}>
-                            <IconTiktok />
-                            TikTok
-                        </div>
-                        <div className={styles.border}></div>
-                    </div>
-                    <div>
-                        <div className={styles.social}>
-                            <IconTwitter />
-                            Twitter
-                        </div>
-                        <div className={styles.border}></div>
-                    </div>
-                </div>
-            </div>
-        </footer>
+            </footer>
+        </div>
     );
 };
 
